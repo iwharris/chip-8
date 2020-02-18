f2dd6b46bcb5ba8e136c44c32f3729940e21fb86
Ian Harris <ianwebsterharris@gmail.com>
2020-02-17 21:24:18 -0700

    Print hex values in uppercase

--- src/chip8/util.ts
+++ src/chip8/util.ts
@@ -9,6 +9,6 @@ const pad = (str: string, digits?: number): string => {
 export const bin = (value: number, padDigits?: number): string => pad(value.toString(2), padDigits);
 
 export const hex = (value: number, padDigits?: number, prefix: boolean = true): string =>
-    (prefix ? '0x' : '') + pad(value.toString(16), padDigits);
+    (prefix ? '0x' : '') + pad(value.toString(16).toUpperCase(), padDigits);
 
 export const reg = (value: number): string => 'V' + hex(value & NIBBLE_MASK, 1, false);
