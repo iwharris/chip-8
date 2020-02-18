378995f3c6331aa462f35a0510b939bb54c5bcba
Ian Harris <ianwebsterharris@gmail.com>
2020-02-17 20:37:10 -0700

    Add util class

---
+++ src/chip8/util.ts
@@ -0,0 +1,14 @@
+import { NIBBLE_MASK } from './const';
+
+const pad = (str: string, digits?: number): string => {
+    digits = digits || str.length;
+
+    return '0'.repeat(digits - str.length) + str;
+};
+
+export const bin = (value: number, padDigits?: number): string => pad(value.toString(2), padDigits);
+
+export const hex = (value: number, padDigits?: number, prefix: boolean = true): string =>
+    (prefix ? '0x' : '') + pad(value.toString(16), padDigits);
+
+export const reg = (value: number): string => 'V' + hex(value & NIBBLE_MASK, 1, false);
