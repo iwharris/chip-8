d85cb6743a2ca9cb569843f6d9de444ad98f1291
Ian Harris <ianwebsterharris@gmail.com>
2020-02-17 20:38:02 -0700

    Update index with simple execution

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export function hello() {
-    return 'Hello world!';
-}
+import { readFileSync } from 'fs';
+import { Emulation } from './chip8/emulation';
+
+const emu = new Emulation({} as any, {} as any, {} as any);
+
+const rom = readFileSync('roms/GUESS');
+
+emu.load(rom);
+
+console.log(emu.dump());
+
+emu.start();
